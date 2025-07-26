fe5f17f483b6e0a2fae27196c6e6751d16dc329c
_ZiolKen <0022446688d@gmail.com>
2025-07-26 12:40:50 +0800

    Delete src directory

--- src/api/status.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const botStatus = await fetch("https://discord-bot-e6f7.onrender.com/status")
-    .then(r => r.json())
-    .catch(() => ({ online: false }));
-
-  res.status(200).json(botStatus);
-}
